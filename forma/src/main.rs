4d1756dba0cb1aa93f10c256331dd4d074778d3f
Max Countryman <maxc@me.com>
2020-05-05 20:55:06 -0700

    provide basic documentation

--- forma/src/main.rs
+++ forma/src/main.rs
@@ -1,3 +1,17 @@
+//! An opinionated SQL formatter
+//!
+//! This provides a command line utility for formatting SQL. Input may be
+//! provided as either a file path or stdin. In the case of the file path, the
+//! original file will be reformatted unless the `--check` flag is provided.
+//! Input from stdin will be output to stdout.
+//!
+//! The companion library, [`formation`], is generalized to be usable in other
+//! contexts.
+//!
+//! [`formation`]: ../formation/index.html
+
+#![deny(clippy::all, missing_docs)]
+
 use std::fs;
 use std::io::{self, Read, Write};
 use std::path::PathBuf;
@@ -23,6 +37,16 @@ struct Opt {
     max_width: usize,
 }
 
+/// Given a writer, writes the given formatted buffers.
+fn write_formatted<W: Write>(mut writer: W, formatted: Vec<String>) -> io::Result<()> {
+    writer.write_all(
+        &formatted
+            .iter()
+            .flat_map(|ps| ps.as_bytes().to_owned())
+            .collect::<Vec<u8>>()[..],
+    )
+}
+
 fn main() -> io::Result<()> {
     let Opt {
         input,
@@ -31,28 +55,21 @@ fn main() -> io::Result<()> {
     } = Opt::from_args();
 
     match input {
+        // `PathBuf` provided, so let's use that.
         Some(input) => {
             let sql_string = fs::read_to_string(&input)?;
             let formatted = format(sql_string, check, max_width)?;
-            let mut writer = fs::File::create(input.clone())?;
-            writer.write_all(
-                &formatted
-                    .iter()
-                    .flat_map(|ps| ps.as_bytes().to_owned())
-                    .collect::<Vec<u8>>()[..],
-            )
+            let writer = fs::File::create(input)?;
+            write_formatted(writer, formatted)
         }
+
+        // Otherwise use stdin and stdout.
         None => {
             let mut sql_string = String::new();
             io::stdin().lock().read_to_string(&mut sql_string)?;
             let formatted = format(sql_string, check, max_width)?;
-            let mut writer = io::stdout();
-            writer.write_all(
-                &formatted
-                    .iter()
-                    .flat_map(|ps| ps.as_bytes().to_owned())
-                    .collect::<Vec<u8>>()[..],
-            )
+            let writer = io::stdout();
+            write_formatted(writer, formatted)
         }
     }
 }
--- formation/src/lib.rs
+++ formation/src/lib.rs
@@ -1,23 +1,44 @@
+//! An opinionated SQL formatter.
+//!
+//! This provides a library which exposes a function [`format`], for
+//! formatting SQL. See the companion binary [`forma`], for a command line
+//! utility that utilitizes this library.
+//!
+//! The style of formatting is intended to be opinionated and so generally not
+//! configurable with the exception of the maximum width.
+//!
+//! Currently `formation` uses a generic SQL dialect that understands
+//! templated strings in the form of `{{...}}`. Configurable dialects may be
+//! added in the future.
+//!
+//! [`format`]: format/fn.format.html
+//! [`forma`]: ../forma/index.html
+
+#![deny(clippy::all, missing_docs)]
 #![feature(box_syntax, box_patterns)]
 use std::io;
-use std::io::Error;
 
-// where should this live
+// TODO: Relocate this?
+/// Forma error type.
 #[derive(Debug)]
 pub enum FormaError {
-    InvalidInput, // "Unable to parse given input as SQL",
-    WouldFormat,  // "Formatting would occur (happens only when check == true)"
-    TransformationFailure(Error),
-    Utf8Failure, // This might want to be more generic, like the stage that it fails at? render failure?
+    /// Unable to parse given input as SQL.
+    InvalidInput,
+    /// Formatting would occur, i.e. when `check` is `true`.
+    WouldFormat,
+    /// A transformation failure that wraps `io::Error`.
+    TransformationFailure(io::Error),
+    /// A UTF8 failure.
+    Utf8Failure,
 }
 
-impl From<FormaError> for Error {
+impl From<FormaError> for io::Error {
     fn from(error: FormaError) -> Self {
         match error {
             FormaError::InvalidInput => io::Error::new(io::ErrorKind::InvalidData, ""),
             FormaError::Utf8Failure => io::Error::new(io::ErrorKind::InvalidData, ""),
             FormaError::WouldFormat => io::Error::new(io::ErrorKind::InvalidData, ""),
-            FormaError::TransformationFailure(err) => err
+            FormaError::TransformationFailure(err) => err,
         }
     }
 }
