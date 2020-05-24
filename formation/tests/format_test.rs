e07d1b598e76402c782f379f3af9762ce2f1d9a0
Max Countryman <maxc@me.com>
2020-05-24 13:09:02 -0700

    rework integration tests (#19)
    * rework integration tests
    
    This changes how integration tests consume their fixtures by adopting a
    the `rstest` library. Doing so enables simple parameterization of the
    tests, making them less repetitive and easier to write.
    
    * fix clippy complaints
    
    * ensure consistent formatting

--- formation/tests/format_test.rs
+++ formation/tests/format_test.rs
@@ -2,141 +2,44 @@ extern crate formation;
 
 use std::fs;
 
+use rstest::rstest;
+
 use pretty_assertions::assert_eq;
 
 const MAX_WIDTH: usize = 100;
 
-#[test]
-fn test_format() {
-    let sql_string = fs::read_to_string("tests/sql/simple.sql").unwrap();
-    assert_eq!(
-        formation::format(sql_string, false, MAX_WIDTH).unwrap(),
-        vec![fs::read_to_string("tests/sql/simple_expected.sql").unwrap()]
-    );
+fn fixture_paths(name: &str) -> (String, String) {
+    (
+        format!("tests/sql/{}.sql", name),
+        format!("tests/sql/{}_expected.sql", name),
+    )
 }
 
-#[test]
-fn test_outer_join() {
-    let sql_string = fs::read_to_string("tests/sql/outer_join.sql").unwrap();
+#[rstest(
+    fixture_paths,
+    case(fixture_paths("between")),
+    case(fixture_paths("case")),
+    case(fixture_paths("correlated_subquery")),
+    case(fixture_paths("ctes")),
+    case(fixture_paths("evaluation_order")),
+    case(fixture_paths("group_by")),
+    case(fixture_paths("inner_join")),
+    case(fixture_paths("natural_join")),
+    case(fixture_paths("null")),
+    case(fixture_paths("outer_join")),
+    case(fixture_paths("simple")),
+    case(fixture_paths("subquery")),
+    case(fixture_paths("join_using")),
+    case(fixture_paths("values")),
+    case(fixture_paths("window_function"))
+)]
+fn test_format(fixture_paths: (String, String)) {
+    let (input_path, expected_path) = fixture_paths;
+    let sql_string = fs::read_to_string(input_path.clone())
+        .unwrap_or_else(|_| panic!("Could not load fixture input path: {}", input_path));
     assert_eq!(
         formation::format(sql_string, false, MAX_WIDTH).unwrap(),
-        vec![fs::read_to_string("tests/sql/outer_join_expected.sql").unwrap()]
-    );
-}
-
-#[test]
-fn test_inner_join() {
-    let sql_string = fs::read_to_string("tests/sql/inner_join.sql").unwrap();
-    assert_eq!(
-        formation::format(sql_string, false, MAX_WIDTH).unwrap(),
-        vec![fs::read_to_string("tests/sql/inner_join_expected.sql").unwrap()]
-    );
-}
-
-#[test]
-fn test_between() {
-    let sql_string = fs::read_to_string("tests/sql/between.sql").unwrap();
-    assert_eq!(
-        formation::format(sql_string, false, MAX_WIDTH).unwrap(),
-        vec![fs::read_to_string("tests/sql/between_expected.sql").unwrap()]
-    );
-}
-
-#[test]
-fn test_subquery() {
-    let sql_string = fs::read_to_string("tests/sql/subquery.sql").unwrap();
-    assert_eq!(
-        formation::format(sql_string, false, MAX_WIDTH).unwrap(),
-        vec![fs::read_to_string("tests/sql/subquery_expected.sql").unwrap()]
-    );
-}
-
-#[test]
-fn test_correlated_subquery() {
-    let sql_string = fs::read_to_string("tests/sql/correlated_subquery.sql").unwrap();
-    assert_eq!(
-        formation::format(sql_string, false, MAX_WIDTH).unwrap(),
-        vec![fs::read_to_string("tests/sql/correlated_subquery_expected.sql").unwrap()]
-    );
-}
-
-#[test]
-fn test_case() {
-    let sql_string = fs::read_to_string("tests/sql/case.sql").unwrap();
-    assert_eq!(
-        formation::format(sql_string, false, MAX_WIDTH).unwrap(),
-        vec![fs::read_to_string("tests/sql/case_expected.sql").unwrap()]
-    );
-}
-
-#[test]
-fn test_evaluation_order() {
-    let sql_string = fs::read_to_string("tests/sql/evaluation_order.sql").unwrap();
-    assert_eq!(
-        formation::format(sql_string, false, MAX_WIDTH).unwrap(),
-        vec![fs::read_to_string("tests/sql/evaluation_order_expected.sql").unwrap()]
-    );
-}
-
-#[test]
-fn test_window_function() {
-    let sql_string = fs::read_to_string("tests/sql/window_function.sql").unwrap();
-    assert_eq!(
-        formation::format(sql_string, false, MAX_WIDTH).unwrap(),
-        vec![fs::read_to_string("tests/sql/window_function_expected.sql").unwrap()]
-    );
-}
-
-#[test]
-fn test_null() {
-    let sql_string = fs::read_to_string("tests/sql/null.sql").unwrap();
-    assert_eq!(
-        formation::format(sql_string, false, MAX_WIDTH).unwrap(),
-        vec![fs::read_to_string("tests/sql/null_expected.sql").unwrap()]
-    );
-}
-
-#[test]
-fn test_group_by() {
-    let sql_string = fs::read_to_string("tests/sql/group_by.sql").unwrap();
-    assert_eq!(
-        formation::format(sql_string, false, MAX_WIDTH).unwrap(),
-        vec![fs::read_to_string("tests/sql/group_by_expected.sql").unwrap()]
-    );
-}
-
-#[test]
-fn test_natural_join() {
-    let sql_string = fs::read_to_string("tests/sql/natural_join.sql").unwrap();
-    assert_eq!(
-        formation::format(sql_string, false, MAX_WIDTH).unwrap(),
-        vec![fs::read_to_string("tests/sql/natural_join_expected.sql").unwrap()]
-    );
-}
-
-#[test]
-fn test_join_using() {
-    let sql_string = fs::read_to_string("tests/sql/join_using.sql").unwrap();
-    assert_eq!(
-        formation::format(sql_string, false, MAX_WIDTH).unwrap(),
-        vec![fs::read_to_string("tests/sql/join_using_expected.sql").unwrap()]
-    );
-}
-
-#[test]
-fn test_values() {
-    let sql_string = fs::read_to_string("tests/sql/values.sql").unwrap();
-    assert_eq!(
-        formation::format(sql_string, false, MAX_WIDTH).unwrap(),
-        vec![fs::read_to_string("tests/sql/values_expected.sql").unwrap()]
-    );
-}
-
-#[test]
-fn test_ctes() {
-    let sql_string = fs::read_to_string("tests/sql/ctes.sql").unwrap();
-    assert_eq!(
-        formation::format(sql_string, false, MAX_WIDTH).unwrap(),
-        vec![fs::read_to_string("tests/sql/ctes_expected.sql").unwrap()]
+        vec![fs::read_to_string(expected_path.clone())
+            .unwrap_or_else(|_| panic!("Could not load fixture expected path: {}", expected_path))]
     );
 }
