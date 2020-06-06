80ba1d0a556745225a8b49d5ceb2f43c5b803bcd
Dandandan <danielheres@gmail.com>
2020-06-07 01:09:28 +0200

    Fix benchmark

--- formation/benches/formation_bench.rs
+++ formation/benches/formation_bench.rs
@@ -5,7 +5,7 @@ fn basic_formatting_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("Formation Formatting");
     let string = "SELECT * FROM bob WHERE 1 = 1";
     group.bench_function("formation::format", |b| {
-        b.iter(|| format(string.to_string(), false, 80));
+        b.iter(|| format(string, false, 80));
     });
 }
 
