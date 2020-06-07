0005f6ebf630ff815c0e242930b238222fb1e641
Max Countryman <maxc@me.com>
2020-06-07 13:04:38 -0700

    provide more complete join coverage

---
+++ formation/tests/sql/cross_join_expected.sql
@@ -0,0 +1,1 @@
+select * from t1 cross join t2;
---
+++ formation/tests/sql/full_join.sql
@@ -0,0 +1,5 @@
+select listing.listid, sum(pricepaid) as price, sum(commission) as comm
+from listing full outer join sales on sales.listid = listing.listid
+where listing.listid between 1 and 5
+group by 1
+order by 1;
