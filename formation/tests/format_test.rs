b006b1cf27735c234268a12e2bf3787346f91ab6
Max Countryman <maxc@me.com>
2020-06-09 17:41:36 -0700

    provide basic exists test

--- formation/tests/format_test.rs
+++ formation/tests/format_test.rs
@@ -26,6 +26,7 @@ fn fixture_paths(name: &str) -> (String, String) {
     case::ctes(fixture_paths("ctes")),
     case::date(fixture_paths("date")),
     case::evaluation_order(fixture_paths("evaluation_order")),
+    case::exists(fixture_paths("exists")),
     case::extract(fixture_paths("extract")),
     case::full_join(fixture_paths("full_join")),
     case::group_by(fixture_paths("group_by")),
