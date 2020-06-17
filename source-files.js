727671a6fbc769a979d3886cd5c66ddad1631553
maxcountryman <maxcountryman@users.noreply.github.com>
2020-06-17 14:02:07 +0000

    Automated deployment: Wed Jun 17 14:02:07 UTC 2020 b1b4f987e984e3646c28678827fc3e4578e528f4

--- source-files.js
+++ source-files.js
@@ -1,4 +1,4 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["forma"] = {"name":"","files":["main.rs"]};
-sourcesIndex["formation"] = {"name":"","dirs":[{"name":"dialect","files":["mod.rs","templated.rs"]}],"files":["doc.rs","error.rs","format.rs","lib.rs"]};
+sourcesIndex["formation"] = {"name":"","dirs":[{"name":"dialect","files":["templated.rs"]},{"name":"doc","files":["body.rs","common.rs","expr.rs","query.rs"]}],"files":["constants.rs","dialect.rs","doc.rs","error.rs","format.rs","lib.rs"]};
 createSourceSidebar();
