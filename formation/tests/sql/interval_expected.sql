469beac842826ac667828f696b00bd8e85838aee
Max Countryman <maxc@me.com>
2020-06-26 08:03:06 -0700

    provide additional test cases

--- formation/tests/sql/interval_expected.sql
+++ formation/tests/sql/interval_expected.sql
@@ -1,2 +1,5 @@
 select interval '01:01.01' minute (5) to second (5);
 select interval '1.1' second (2, 2);
+select interval '1' year;
+select interval '1' day to second;
+select interval '1' second (1);
